d1b30a9be1a6e5da6566d7c62cb858dded37635f
ownesis <83978267+ownesis@users.noreply.github.com>
2021-08-22 21:00:42 +0200

    Update example.c

--- example.c
+++ example.c
@@ -32,16 +32,15 @@ int main(void) {
     cqueue_t *cqueue = cqueue_create(3);
    
     printf("[+] Add a const string\n");
-    ret = cqueue_write(cqueue, (void *)"Hello");
+    ret = cqueue_push(cqueue, "Hello");
     ERR(ret);
 
     printf("[+] Add a string\n");
-    //ret = cqueue_write(cqueue, (void *)ptr2);
     ret = cqueue_push(cqueue, ptr2);
     ERR(ret);
  
     printf("[+] Add a integer\n\n");
-    ret = cqueue_write(cqueue, (void *)&val);
+    ret = cqueue_push(cqueue, &val);
     ERR(ret);
     
     printf("[i] size of queue: %ld\n", CQUEUE_GET_SIZE(cqueue));
@@ -51,21 +50,21 @@ int main(void) {
              : "nop");
 
     printf("[+] Read the fisrt element added\n");
-    ret = cqueue_read(cqueue, (void *)&ptr2);
+    ret = cqueue_pop(cqueue, &ptr2);
     ERR(ret);
 
     printf("[i] ptr2 = %s\n", ptr2);
     printf("[i] size of queue: %ld\n\n", CQUEUE_GET_SIZE(cqueue));
   
     printf("[+] Read the second element added\n");
-    ret = cqueue_read(cqueue, (void *)&ptr);
+    ret = cqueue_pop(cqueue, &ptr);
     ERR(ret);
     
     printf("[i] ptr = %s\n", ptr);
     printf("[i] size of queue: %ld\n\n", CQUEUE_GET_SIZE(cqueue));
 
     printf("[+] Read until remove the third element added\n");
-    ret = cqueue_peek(cqueue, (void *)&ptr_val);
+    ret = cqueue_get(cqueue, &ptr_val);
     ERR(ret);
     
     *ptr_val += 2; 
